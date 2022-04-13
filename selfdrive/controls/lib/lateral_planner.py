8b2bb96a24ffd3ee5446920fe11ed9eedfe8d9db
Chanho Jung <jungch@naver.com>
2022-04-13 15:34:53 +0900

    set laneless minimum heading cost

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -127,13 +127,13 @@ class LateralPlanner:
     elif self.laneless_mode == 1: # Laneless Mode(1)
       d_path_xyz = self.path_xyz
       path_cost = np.clip(abs(self.path_xyz[0, 1] / self.path_xyz_stds[0, 1]), self.path_cost[0], self.path_cost[1]) * MPC_COST_LAT.PATH
-      heading_cost = self.heading_cost
+      heading_cost = interp(v_ego, [5.0, 10.0], [MPC_COST_LAT.HEADING, MPC_COST_LAT.LANELESS_HEADING_MIN])
       self.lat_mpc.set_weights(path_cost, heading_cost, self.steer_rate_cost)
       self.laneless_mode_status = True
     elif self.laneless_mode == 2 and ((self.LP.lll_prob + self.LP.rll_prob)/2 < 0.3) and self.DH.lane_change_state == LaneChangeState.off:
       d_path_xyz = self.path_xyz
       path_cost = np.clip(abs(self.path_xyz[0, 1] / self.path_xyz_stds[0, 1]), self.path_cost[0], self.path_cost[1]) * MPC_COST_LAT.PATH
-      heading_cost = self.heading_cost
+      heading_cost = interp(v_ego, [5.0, 10.0], [MPC_COST_LAT.HEADING, MPC_COST_LAT.LANELESS_HEADING_MIN])
       self.lat_mpc.set_weights(path_cost, heading_cost, self.steer_rate_cost)
       self.laneless_mode_status = True
       self.laneless_mode_status_buffer = True
@@ -146,7 +146,7 @@ class LateralPlanner:
     elif self.laneless_mode == 2 and self.laneless_mode_status_buffer == True and self.DH.lane_change_state == LaneChangeState.off:
       d_path_xyz = self.path_xyz
       path_cost = np.clip(abs(self.path_xyz[0, 1] / self.path_xyz_stds[0, 1]), self.path_cost[0], self.path_cost[1]) * MPC_COST_LAT.PATH
-      heading_cost = self.heading_cost
+      heading_cost = interp(v_ego, [5.0, 10.0], [MPC_COST_LAT.HEADING, MPC_COST_LAT.LANELESS_HEADING_MIN])
       self.lat_mpc.set_weights(path_cost, heading_cost, self.steer_rate_cost)
       self.laneless_mode_status = True
     else:
